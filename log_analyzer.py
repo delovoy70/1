e1e0f0ac1c608f43c6889934511469d28a87c180
Alex <malem@bk.ru>
2019-11-13 13:18:40 +0300

    major changes

--- log_analyzer.py
+++ log_analyzer.py
@@ -7,16 +7,18 @@
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
 import argparse
-import gzip
-import re
-import os
+import collections
 import datetime
+import gzip
+import json
 import logging
-import pandas as pd
+import os
+import re
+import statistics
+import sys
 from shutil import copyfile
 from string import Template
-import yaml
-import sys
+from typing import Dict
 
 config = {
     "REPORT_SIZE": 1000,
@@ -24,8 +26,15 @@ config = {
     "LOG_DIR": "./log"
 }
 
+logging.basicConfig(
+    format='[%(asctime)s] %(levelname).1s %(message)s',
+    datefmt='%Y.%m.%d %H:%M:%S',
+    level=logging.DEBUG,
+    filename=None,
+)
+
 # lexeme types
-WSP, QUOTED_STRING, DATE, RAW, NO_DATA = range(5) # ENUM
+WSP, QUOTED_STRING, DATE, RAW, NO_DATA = range(5)  # ENUM
 
 RULES = [
     ('\s+', WSP),
@@ -40,16 +49,6 @@ class LogEntry(object):
     __slots__ = ('remote_addr', 'remote_user', 'http_x_real_ip', 'time_local', 'request', 'status',
               'body_bytes_sent', 'http_referer', 'http_user_agent', 'http_x_forwarded_for',
               'http_X_REQUEST_ID', 'http_X_RB_USER', 'request_time')
-
-    def to_dict(self):
-        return {
-            'url': self.request.split()[1],
-            'request_time': self.request_time,
-            'count': self.request.split()[1],
-            'time_sum': float(self.request_time),
-            'time_max': float(self.request_time),
-            'time_med': float(self.request_time),
-        }
 
 
 def lexer(rules):
@@ -72,7 +71,7 @@ def lexer(rules):
 
 def find_newest_log(log_dir):
 
-    pattern_file = r'nginx-access-ui.log-+\d{8}\.gzip|'
+    pattern_file = r'^nginx-access-ui\.log-(\d{8})(\.gz|\.log)'
     files = os.listdir(log_dir)
     max_date = datetime.date(1, 1, 1)
     log_name = ''
@@ -120,7 +119,7 @@ def read_log(log_name):
                 elif token_type == NO_DATA:
                     value = None  # NO_DATA заменяем на None
                 elif token_type == RAW:
-                    value = re_match.group(1)  # MatchObject.group(i) возвращает i-ую заключённую в круглые скобки группу
+                    value = re_match.group(1)  # group(i) возвращает i-ую заключённую в круглые скобки группу
                 elif token_type == QUOTED_STRING:
                     value = re_match.group(1)  # снимаем экранирование с заэкранированных кавычек
                 elif token_type == DATE:
@@ -131,44 +130,57 @@ def read_log(log_name):
                 field_name = LogEntry.__slots__[field_idx]
                 setattr(entry, field_name, value)
                 field_idx += 1
-            if len(entry.request.split()) > 1:
-                result.append(entry)
 
     return result
 
 
-def process_data(log_data, report_size):
-    pd_log_data = pd.DataFrame.from_records([ld.to_dict() for ld in log_data])
-    pd_log_data1 = pd_log_data.groupby('url').agg({'count': 'count'}) \
-        .join(pd_log_data.groupby('url').agg({'time_sum': 'sum'})) \
-        .join(pd_log_data.groupby('url').agg({'time_max': 'max'})) \
-        .join(pd_log_data.groupby('url').agg({'time_med': 'median'}))
+def process_data(log_data, report_size, errors_level):
 
-    total_count = pd_log_data1['count'].sum()
-    total_time = pd_log_data1['time_sum'].sum()
+    dict_data: Dict[str, list[float]] = collections.defaultdict(list)
+    lines, errors = 0, 0
 
-    pd_log_data1 = pd_log_data1.sort_values(by=['time_sum'], ascending=False).head(report_size)
+    for i in log_data:
+        lines += 1
+        try:
+            url = i.request.split()[1]
+        except:
+            errors += 1
+            continue
 
-    count_perc = []
-    time_perc = []
-    time_avg = []
-    time_sum = []
-    time_med = []
+        dict_data[url].append(float(i.request_time))
 
-    for index, row in pd_log_data1.iterrows():
-        count_perc.append(round(row['count'] * 100 / total_count, 3))
-        time_perc.append(round(row['time_sum'] * 100 / total_time, 3))
-        time_avg.append(round(row['time_sum'] / row['count'], 3))
-        time_sum.append(round(row['time_sum'], 3))
-        time_med.append(round(row['time_med'], 3))
+    if errors / lines > errors_level:
+        logging.WARNING(f'Too much errors: {errors} errors from {lines} rows')
+        return []
 
-    pd_log_data1['count_perc'] = count_perc
-    pd_log_data1['time_perc'] = time_perc
-    pd_log_data1['time_avg'] = time_avg
-    pd_log_data1['time_sum'] = time_sum
-    pd_log_data1['time_med'] = time_med
+    new_dict = {}
+    arr = []
+    for i in sorted(dict_data.items(), reverse=True, key=lambda x: sum(x[1])):
+        arr.append(i[0])
+    dict_data1 = new_dict.fromkeys(arr)
 
-    return pd_log_data1
+    for i in dict_data1.keys():
+        dict_data1[i] = dict_data[i]
+
+    data_for_report = []
+    count, time = 0, 0
+    for request_times in dict_data1.values():
+        count += len(request_times)
+        time += sum(request_times)
+
+    for url, request_times in dict_data1.items():
+        data_for_report.append({
+            'url': url,
+            'count': len(request_times),
+            'count_perc': round(100 * len(request_times) / float(count), 3),
+            'time_sum': round(sum(request_times), 3),
+            'time_perc': round(100 * sum(request_times) / time, 3),
+            'time_avg': round(statistics.mean(request_times), 3),
+            'time_max': round(max(request_times), 3),
+            "time_med": round(statistics.median(request_times), 3),
+        })
+
+    return data_for_report[:report_size]
 
 
 def main():
@@ -179,30 +191,44 @@ def main():
     if args.config:
         try:
             with open(args.config, 'r') as f:
-                file_conf = yaml.load(f, Loader=yaml.Loader)
+                file_conf = json.load(f)
                 log_dir = file_conf['LOG_DIR'] if 'LOG_DIR' in file_conf.keys() else config['LOG_DIR']
                 report_size = file_conf['REPORT_SIZE'] if 'REPORT_SIZE' in file_conf.keys() else config['REPORT_SIZE']
                 report_dir = file_conf['REPORT_DIR'] if 'REPORT_DIR' in file_conf.keys() else config['REPORT_DIR']
+                errors_level = file_conf['ERRORS_LEVEL'] if 'ERRORS_LEVEL' in file_conf.keys() else None
         except:
+            logging.WARNING('An error occurred while parsing the config file')
             sys.exit()
     else:
         log_dir = config['LOG_DIR']
         report_size = config['REPORT_SIZE']
         report_dir = config['REPORT_DIR']
+        errors_level = None
 
     log_params = find_newest_log(log_dir)
+
+    if not log_params:
+        logging.info('There are no logs to proceed')
+        sys.exit()
+
     log_date = log_params['log_date']
     report_name = 'report-' + log_date.strftime('%Y.%m.%d') + '.html'
 
     files = os.listdir(report_dir)
     if report_name in files:
-        print('Report ' + report_name + ' already exist. Abort proceeding logs')
+        logging.info('Report ' + report_name + ' already exist. Abort proceeding logs')
         sys.exit()
+
     log_name = log_params['log_name']
 
     log_data = read_log(log_name)
-    processed_data = process_data(log_data, report_size)
-    processed_data = processed_data.reset_index().to_json(orient='records', index=True)
+    processed_data = process_data(log_data, report_size, errors_level)
+
+    if not processed_data:
+        logging.info('Nothing to put into the report')
+        sys.exit()
+
+    processed_data = json.dumps(processed_data)
 
     report_path = '/'.join([report_dir, report_name])
     copyfile('report.html', report_path)
@@ -219,4 +245,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except:
+        logging.exception('Unexpected exception')
